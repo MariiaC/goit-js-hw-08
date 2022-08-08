5e51ce7937122a4f086a8f754e86456756a21b5f
MariiaC <mariiachvorun@gmail.com>
2022-08-08 22:32:15 +0300

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,60 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Add imports above this line
+
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const itemsMarkup = galleryItemsMarkup(galleryItems); //передаэмо галерею
+
+galleryRef.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function galleryItemsMarkup(items) {
+   
+    return items.map(({preview, original, description}) => {
+    
+    return `<div class="gallery__item">
+    <a class='gallery__link' href='${original}'>
+    <img class='gallery__image' src='${preview}' data-source='${original}'
+    alt='${description}'/>
+    </a>
+    </div>`
+    })
+    .join('');
+};
+
+galleryRef.addEventListener('click', galleryRefAddContainerClick)
+
+function galleryRefAddContainerClick(event) {
+    
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    event.preventDefault();
+    // console.log(event.target);
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}"/>`, {
+        onShow: () => {
+            window.addEventListener('keydown', keyDown)
+        },
+        onClose: () => {
+            window.removeEventListener('keydown', keyDown)
+        }
+    }
+    );
+    function keyDown(event) {
+
+        if (event.code === 'Escape') {
+            instance.close()
+        }
+    }
+
+instance.show()
+}
+
+
