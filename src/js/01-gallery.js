249e10f96688571e15921bec9979dff935c9c56f
MariiaC <mariiachvorun@gmail.com>
2022-08-10 00:39:59 +0300

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -10,51 +10,23 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 //console.log(galleryItems);
 
+//код нижче просто вставляємо з домашки М7, завдання 2
+
 const galleryRef = document.querySelector('.gallery');
 const itemsMarkup = galleryItemsMarkup(galleryItems); //передаэмо галерею
 
 galleryRef.insertAdjacentHTML('beforeend', itemsMarkup);
 
 function galleryItemsMarkup(items) {
-   
-    return items.map(({preview, original, description}) => {
-    
-    return `<div class="gallery__item">
-    <a class='gallery__link' href='${original}'>
-    <img class='gallery__image' src='${preview}' data-source='${original}'
-    alt='${description}'/>
-    </a>
-    </div>`
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div>
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a> </div>
+    `;
     })
     .join('');
-};
-
-galleryRef.addEventListener('click', galleryRefAddContainerClick)
-
-function galleryRefAddContainerClick(event) {
-    
-    if (!event.target.classList.contains('gallery__image')) {
-        return;
-    }
-    event.preventDefault();
-    // console.log(event.target);
-    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}"/>`, {
-        onShow: () => {
-            window.addEventListener('keydown', keyDown)
-        },
-        onClose: () => {
-            window.removeEventListener('keydown', keyDown)
-        }
-    }
-    );
-    function keyDown(event) {
-
-        if (event.code === 'Escape') {
-            instance.close()
-        }
-    }
-
-instance.show()
 }
 
-
+ const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from "lodash.throttle";
+
+const formRef = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {}//виводь у консоль об'єкт з полями email, message
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+getForm();
+
+
+function onFormInput(event) {
+    // console.log(event.target.name);
+    // console.log(event.target.value);
+
+    formData[event.target.name] = event.target.value;
+    // console.log(formData);
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+
+    // console.log(formData);
+
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    const parsedData = JSON.parse(savedData);
+    console.log('parsedData', parsedData);
+    
+};
+
+function onFormSubmit (event) {
+    event.preventDefault();
+    //console.log('send form');//перевірка чи перезавант при submit
+    event.target.reset(); //очищуємо форму-інтерфейс(поля)
+    localStorage.removeItem(STORAGE_KEY);//очищуємо локал сторедж
+};
+
+function getForm() {
+    const savedMsg = localStorage.getItem(STORAGE_KEY);
+    if (savedMsg) {
+        formRef.value=savedMsg;//якщо є такий ключ, то збережеться у формі , в ін випадку -нул
+    }
+
+    // console.log(savedMsg);
+};
 
